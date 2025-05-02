26257d1be4e17e2b95deae248b503ac6dec6ec9b
karim-abdelrhman <kabdo2536@gmail.com>
2025-05-02 08:28:09 +0300

    add image to users

--- app/Livewire/CreateNewUser.php
+++ app/Livewire/CreateNewUser.php
@@ -5,11 +5,12 @@ namespace App\Livewire;
 use App\Models\User;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 use Livewire\WithPagination;
 
 class CreateNewUser extends Component
 {
-    use withPagination;
+    use withPagination , withFileUploads;
 
     #[validate('required' , message: 'please enter a name')]
     #[validate('min:3' , message: 'please note that the name must be at least 3 characters long')]
@@ -22,17 +23,24 @@ class CreateNewUser extends Component
     #[validate('required' , message: 'please enter a password')]
     #[validate('min:8' , message: 'please note that the password must be at least 8 characters long')]
     public $password;
+
+    #[validate('image' , message: 'please upload an image')]
+    #[validate('mimes:jpg,jpeg,png' , message: 'please upload a valid image')]
+    public $image;
     public function createUser()
     {
         $validatedData = $this->validate();
+        if($this->image){
+            $validatedData['image'] = $this->image->store('users' , 'public');;
+        }
         User::create($validatedData);
-        $this->reset(['name', 'email', 'password']);
+        $this->reset(['name', 'email', 'password' , 'image']);
         flash()->success('User saved successfully!');
     }
     public function render()
     {
         return view('livewire.create-new-user' , [
-            'users' => User::paginate(3),
+            'users' => User::orderBy('id', 'DESC')->paginate(3),
         ]);
     }
 }
--- resources/views/livewire/create-new-user.blade.php
+++ resources/views/livewire/create-new-user.blade.php
@@ -1,6 +1,6 @@
 <div>
-    <div class="row">
-        <div class="col-md-12">
+    <div class="row mt-3">
+        <div class="col-md-4">
             <div class="card">
                 <div class="card-header">
                     <h3 class="card-title">Create new user</h3>
@@ -8,32 +8,49 @@
                 <div class="card-body">
                     <form wire:submit="createUser">
                         <div class="row">
-                            <div class="col-md-4">
-                                <label for="">Name</label>
+                            <div class="col-md-12">
+                                <label for="" class="col-form-label">Name</label>
                                 <input class="form-control col-md-4" type="text" name="name" wire:model="name">
-                                @error('name') <div class="alert alert-danger">{{$message}}</div> @enderror
+                                @error('name')
+                                <div class="alert alert-danger">{{$message}}</div> @enderror
                             </div>
-                            <div class="col-md-4">
-                                <label for="">Email</label>
-                                <input class="form-control col-md-4" type="email" name="email" wire:model="email">
-                                @error('email') <div class="alert alert-danger">{{$message}}</div> @enderror
+                            <div class="col-md-12 mt-3">
+                                <label for="email" class="col-form-label">Email</label>
+                                <input id="email" class="form-control form-group col-md-4" type="email" name="email"
+                                       wire:model="email">
+                                @error('email')
+                                <div class="alert alert-danger">{{$message}}</div> @enderror
                             </div>
-                            <div class="col-md-4">
-                                <label for="">Password</label>
-                                <input class="form-control col-md-4" type="password" name="name" wire:model="password">
-                                @error('password') <div class="alert alert-danger">{{$message}}</div> @enderror
+                            <div class="col-md-12 mt-3">
+                                <label for="" class="col-form-label">Upload Image</label>
+                                <input class="form-control" type="file" name="image" wire:model="image">
+                                @error('image')
+                                <div class="alert alert-danger">{{$message}}</div> @enderror
+
+                                @if($image)
+                                    <img src="{{$image->temporaryUrl()}}" alt="" width="100px" class="mt-3">
+                                @endif
+
+                                <div wire:loading wire:target="image">
+                                    <span class="text-success"> uploading .... </span>
+                                </div>
                             </div>
+                            <div class="col-md-12 mt-3">
+                                <label for="" class="col-form-label">Password</label>
+                                <input class="form-control col-md-4" type="password" name="name"
+                                       wire:model="password">
+                                @error('password')
+                                <div class="alert alert-danger">{{$message}}</div> @enderror
+                            </div>
+                            <button type="submit" class="btn btn-success mt-3 col-md-12">
+                                create new user
+                            </button>
                         </div>
-                        <button type="submit" class="btn btn-success mt-3" >
-                            create new user
-                        </button>
                     </form>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="row mt-3">
-        <div class="col-md-12">
+        <div class="col-md-8">
             <div class="card">
                 <div class="card-header">
                     <h3 class="card-title">All users</h3>
@@ -42,9 +59,10 @@
                     <table class="table table-bordered">
                         <thead>
                         <tr>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Action</th>
+                            <th class="text-center">Name</th>
+                            <th class="text-center">Email</th>
+                            <th class="text-center">Image</th>
+                            <th class="text-center">Action</th>
                         </tr>
                         </thead>
                         <tbody>
@@ -52,6 +70,12 @@
                             <tr>
                                 <td>{{$user->name}}</td>
                                 <td>{{$user->email}}</td>
+                                <td>
+                                    @if($user->image)
+                                        <img src="{{asset('storage/' . $user->image)}}" alt="user image"
+                                             style="width: 75px">
+                                    @endif
+                                </td>
                                 <td>
                                     <button class="btn btn-danger">Delete</button>
                                     <button class="btn btn-success">Edit</button>
