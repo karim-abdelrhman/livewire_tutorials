a316809002b20bc6a8f19ec86c039cee45e675ef
karim-abdelrhman <kabdo2536@gmail.com>
2025-05-03 15:09:10 +0300

    implement search function

--- app/Livewire/UserLists.php
+++ app/Livewire/UserLists.php
@@ -7,10 +7,12 @@ use Livewire\Component;
 
 class UserLists extends Component
 {
+    public $search;
+
     public function render()
     {
         return view('livewire.user-lists' , [
-            'users' => User::orderBy('id', 'DESC')->paginate(3),
+            'users' => User::where('name' , 'like' , "%{$this->search}%")->paginate(3),
         ]);
     }
 }
